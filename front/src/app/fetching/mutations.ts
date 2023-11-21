53b62daed005c060342a171fb5c4742610d2c929
Franz <softymodel@gmail.com>
2023-11-21 23:40:36 +0300

    mutations fixed

--- front/src/app/fetching/mutations.ts
+++ front/src/app/fetching/mutations.ts
@@ -1,11 +1,20 @@
 import axios from "axios";
 
-type request="POST"|"PUT"
+type request="POST"|"PUT"|"PATCH"
+
+
 
 class Mutate extends OptionsBase{
+    
     private type:request;
     private data:JSON;
     private validate:boolean;
+
+    private readonly actions={
+        "POST": this.postData(),
+        "PUT": this.putData(),
+        "PATCH":this.patchData(),
+    }
 
     constructor (url:string,type:request,data:JSON,validate:boolean,params?:JSON){
     super(url,params)
@@ -14,7 +23,7 @@ class Mutate extends OptionsBase{
     this.validate=validate;
 }
 
-    private postData(){
+    private postData() {
     axios.post(this.configurator(),this.data).then((response)=>{
       return response
     })
@@ -23,7 +32,7 @@ class Mutate extends OptionsBase{
     })
     }
 
-    private putData(){
+    private putData() {
     return axios.put(this.configurator(),this.data).then((response)=>{
         return response
     }).catch((err)=>{
@@ -31,12 +40,17 @@ class Mutate extends OptionsBase{
     })
     }
 
-    public sendMutation(){
-        return this.type==="POST"?
-        this.postData():
-        this.putData()
+    private patchData() {
+        return axios.patch(this.configurator(),this.data).then((response)=>{
+            return response
+        }).catch((err)=>{
+            return err;
+        })
     }
+    
 
-
+    public async sendMutation(){
+        return await this.actions[this.type]
+    }
 
 }
