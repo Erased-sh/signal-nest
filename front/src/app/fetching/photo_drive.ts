fab38334c908a0568c8525c9a5736f94e6938022
Franz <softymodel@gmail.com>
2023-12-17 01:12:18 +0300

    yandex disc test passed

--- front/src/app/fetching/photo_drive.ts
+++ front/src/app/fetching/photo_drive.ts
@@ -1,41 +1,5 @@
-import { google } from 'googleapis';
 
 export const getPhotosDrive = async () => {
-  const keyFilePath = 'front/noble-beanbag-408304-849a2d948148.json';
-  const keys = require(keyFilePath);
-
-  const auth = new google.auth.GoogleAuth({
-    credentials: {
-      client_email: keys.client_email,
-      private_key: keys.private_key,
-    },
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-  });
-
-  const drive = google.drive({ version: 'v3', auth });
-
-
-    const res = await drive.files.list({
-      q: "'root' in parents and name='photos' and mimeType='image/jpeg'"
-    });
-    
-
-    const files = res.data.files;
-    if (files?.length) {
-      const urls = await Promise.all(files.map(async (file) => {
-      
-          const fileInfo = await drive.files.get({
-            fileId: file.id || '',
-            fields: 'webContentLink',
-          });
-          console.error(fileInfo.data.webContentLink)
-          return fileInfo.data.webContentLink || '';
-        
-      }));
-      return urls.filter(url => url !== '');
-    } else {
-      console.log('No files found.');
-      return [];
-    }
+  const token="OAuth y0_AgAAAABy2V8BAAsAZAAAAAD1A5NVkdB2Wbr0R1iwF7Uxrn-VPbP0H8U"
   
 };
--- front/src/app/photos/page.tsx
+++ front/src/app/photos/page.tsx
@@ -1,44 +1,58 @@
 
-export async function generateStaticParams(){
-    try {
-      const urls:string[]= await getPhotosDrive();
-      console.log(urls)
-      return {props:
-        {urls}
-    }
-    } catch (error) {
-      const urls:string[]=[""]
-      console.error('Error fetching photos:', error);
-      return {props:
-        {urls}
-    }
-    }
-  };
+import axios from 'axios';
+
+const token = "y0_AgAAAABy2V8BAAsAZAAAAAD1A5NVkdB2Wbr0R1iwF7Uxrn-VPbP0H8U";
+interface Prew{
+   
+    preview:string
+   
+}
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://cloud-api.yandex.net/v1/disk/resources/public', {
+      headers: {
+        Authorization: `OAuth ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    
+    const keys=Object.keys(response.data.items)
+
+    const data:string[]=keys.map((key)=>{
+       return response.data.items[key].file
+    })
+    console.log(data)
+   
+    return data
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+    const data=[]
+    return 
+  }
+};
+
 
 
 import style from "../page.module.css"
 import NavigationBar from "../components/layout/navigation_bar"
 import FooterBar from "../components/layout/footer_bar"
 import PhotoGrid from "../components/photogrid/photo_grid"
-import { getPhotosDrive } from "../fetching/photo_drive";
-interface pr{
-    urls:string[]
-}
 
-export default async function PhotosPage({urls}:pr){
+
+
+export default async function PhotosPage(){
     
     const photos_example = [[1,2,3,4], [4,5,6,7], [8,9,10,10] ]
+    const fetch=await fetchData()||[""]
     return (
         <div className={style.body}>
             <NavigationBar></NavigationBar>
 
-            {
 
-                urls?.map((photo)=>{
-                    return <img src={photo} alt={photo}/>
-                })
+         
                 
-            }
+            
 
             <div className={style.main}>
 
@@ -72,8 +86,11 @@ export default async function PhotosPage({urls}:pr){
                         source4={`/photos/${photo[3]}.jpg`}
                         >
                         </PhotoGrid>
-                        <img src="https://drive.google.com/file/d/1PXz227-kJS0KXXI_H38WLRxWeYqdZdN5/view?usp=share_link"/>
+                        
 
+                        {fetch.map((file)=>{
+            return <img height="100" width="100" src={file} alt="nor"></img>
+            })}
                     <div className={style.col2}></div>
                 </div>
                 )}
